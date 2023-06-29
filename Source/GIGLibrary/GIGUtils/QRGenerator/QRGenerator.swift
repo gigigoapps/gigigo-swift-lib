71d2c42ceaa9d869de485d35c73fe01eb86cb90f
Nacho Brambilla <nacho@dev33.mobi>
2023-06-29 12:50:02 +0200

    Fix: Use CGImage instead CIImage

--- Source/GIGLibrary/GIGUtils/QRGenerator/QRGenerator.swift
+++ Source/GIGLibrary/GIGUtils/QRGenerator/QRGenerator.swift
@@ -13,57 +13,54 @@ import CoreImage.CIFilterBuiltins
 open class QR {
     
 	open class func generate(_ string: String) -> UIImage? {
-		guard let outputImage: CIImage = self.generate(string) else { return nil }
-		let image = UIImage(ciImage: outputImage)
+		guard let outputImage: CGImage = self.generate(string) else { return nil }
+		let image = UIImage(cgImage: outputImage)
 		
 		return image
 	}
 	
 	open class func generate(_ string: String, onView: UIImageView) {
-		guard let image: CIImage = self.generate(string) else { return }
+		guard let image: CGImage = self.generate(string) else { return }
 		
-		let scaleX = onView.frame.size.width / image.extent.size.width
-		let scaleY = onView.frame.size.height / image.extent.size.height
-		
-		let transformedImage = image.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
-		
-		onView.image = UIImage(ciImage: transformedImage)
-	}
-    
-    open class func create(string: String) -> UIImage {
-        if #available(iOS 13.0, *) {
-            let context = CIContext()
-            let filter = CIFilter.qrCodeGenerator()
-            filter.message = Data(string.utf8)
-            guard let outputImage = filter.outputImage,
-                  let cgimg = context.createCGImage(outputImage, from: outputImage.extent) else {
-                return UIImage()
-            }
-            return UIImage(cgImage: cgimg)
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+        guard let context = CGContext(
+            data: nil,
+            width: Int(onView.frame.size.width),
+            height: Int(onView.frame.size.height),
+            bitsPerComponent: image.bitsPerComponent,
+            bytesPerRow: 0,
+            space: image.colorSpace ?? CGColorSpaceCreateDeviceRGB(),
+            bitmapInfo: bitmapInfo.rawValue) else { return }
+
+        context.interpolationQuality = .high
+        context.draw(image, in: CGRect(origin: .zero, size: onView.frame.size))
+        if let cgImage = context.makeImage() {
+            onView.image = UIImage(cgImage: cgImage)
         }
-        
-        return UIImage()
-    }
+	}
 	
 	// MARK: - Private Helpers
 	
-	fileprivate class func generate(_ string: String) -> CIImage? {
+	fileprivate class func generate(_ string: String) -> CGImage? {
+        let context = CIContext()
         if #available(iOS 13.0, *) {
-            let context = CIContext()
             let filter = CIFilter.qrCodeGenerator()
             filter.message = Data(string.utf8)
             guard let outputImage = filter.outputImage,
                   let cgimg = context.createCGImage(outputImage, from: outputImage.extent) else {
                     return nil
             }
-            return CIImage(cgImage: cgimg)
+            return cgimg
         } else {
             let stringData = string.data(using: String.Encoding.utf8)
             let filter = CIFilter(name: "CIQRCodeGenerator")
             filter?.setValue(stringData, forKey: "inputMessage")
             filter?.setValue("H", forKey: "inputCorrectionLevel")
-            
-            return filter?.outputImage
+            guard let outputImage = filter?.outputImage,
+                  let cgimg = context.createCGImage(outputImage, from: outputImage.extent) else {
+                    return nil
+            }
+            return cgimg
         }
 	}
 }
