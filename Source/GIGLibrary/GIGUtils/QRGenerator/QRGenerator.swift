b3ff5b9e9359dc825c188e16c27e56b65afcda25
Nacho Brambilla <nacho@dev33.mobi>
2023-06-29 12:09:25 +0200

    Fix:  Control iOS 13 availability

--- Source/GIGLibrary/GIGUtils/QRGenerator/QRGenerator.swift
+++ Source/GIGLibrary/GIGUtils/QRGenerator/QRGenerator.swift
@@ -13,33 +13,42 @@ import CoreImage.CIFilterBuiltins
 open class QR {
     
 	open class func generate(_ string: String) -> UIImage? {
-		guard let outputImage: CGImage = self.generate(string) else { return nil }
-		let image = UIImage(cgImage: outputImage)
+		guard let outputImage: CIImage = self.generate(string) else { return nil }
+		let image = UIImage(ciImage: outputImage)
 		
 		return image
 	}
 	
 	open class func generate(_ string: String, onView: UIImageView) {
-		guard let image: CGImage = self.generate(string) else { return }
+		guard let image: CIImage = self.generate(string) else { return }
 		
 		let scaleX = onView.frame.size.width / image.extent.size.width
 		let scaleY = onView.frame.size.height / image.extent.size.height
 		
 		let transformedImage = image.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
 		
-		onView.image = UIImage(cgImage: transformedImage)
+		onView.image = UIImage(ciImage: transformedImage)
 	}
 	
 	// MARK: - Private Helpers
 	
 	fileprivate class func generate(_ string: String) -> CIImage? {
-        let context = CIContext()
-        let filter = CIFilter.qrCodeGenerator()
-        filter.message = Data(string.utf8)
-        guard let outputImage = filter.outputImage,
-              let cgimg = context.createCGImage(outputImage, from: outputImage.extent) else {
-                return nil
+        if #available(iOS 13.0, *) {
+            let context = CIContext()
+            let filter = CIFilter.qrCodeGenerator()
+            filter.message = Data(string.utf8)
+            guard let outputImage = filter.outputImage,
+                  let cgimg = context.createCGImage(outputImage, from: outputImage.extent) else {
+                    return nil
+            }
+            return CIImage(cgImage: cgimg)
+        } else {
+            let stringData = string.data(using: String.Encoding.utf8)
+            let filter = CIFilter(name: "CIQRCodeGenerator")
+            filter?.setValue(stringData, forKey: "inputMessage")
+            filter?.setValue("H", forKey: "inputCorrectionLevel")
+            
+            return filter?.outputImage
         }
-        return cgimg
 	}
 }
