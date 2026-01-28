0ee6deedfd0da8ee1e27163b71288ed9e80a8130
Alejandro Jiménez <a.j.agudo@gmail.com>
2026-01-28 11:23:41 +0100

    Simplify active window lookup

--- Sources/GIGLibrary/GIGUtils/Application/UIApplication+Window.swift
+++ Sources/GIGLibrary/GIGUtils/Application/UIApplication+Window.swift
@@ -10,21 +10,17 @@ import UIKit
 
 extension UIApplication {
     var activeWindow: UIWindow? {
-        if #available(iOS 13.0, *) {
-            let activeScenes = connectedScenes
-                .filter { $0.activationState == .foregroundActive }
-                .compactMap { $0 as? UIWindowScene }
-            let activeWindows = activeScenes.flatMap { $0.windows }
-            if let window = activeWindows.first(where: { $0.isKeyWindow }) ?? activeWindows.first {
-                return window
-            }
-
-            let windows = connectedScenes
-                .compactMap { $0 as? UIWindowScene }
-                .flatMap { $0.windows }
-            return windows.first { $0.isKeyWindow } ?? windows.first
+        let activeScenes = connectedScenes
+            .filter { $0.activationState == .foregroundActive }
+            .compactMap { $0 as? UIWindowScene }
+        let activeWindows = activeScenes.flatMap { $0.windows }
+        if let window = activeWindows.first(where: { $0.isKeyWindow }) ?? activeWindows.first {
+            return window
         }
 
-        return keyWindow
+        let windows = connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .flatMap { $0.windows }
+        return windows.first { $0.isKeyWindow } ?? windows.first
     }
 }
